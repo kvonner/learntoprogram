b24a25008b75b318d532b6dc3feefbafb3964605
Kristian von Rickenbach <kvonrickenbach@gmail.com>
2013-05-05 13:21:06 -0700

    Including chapter 3 code

--- calc.rb
+++ calc.rb
@@ -2,3 +2,20 @@ puts 365*24
 puts 10*365*24*60
 puts 27*365*24*60*60
 puts 1111000000 / 60 / 60 / 24 / 365
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts "I like" + " apple pie"
+puts "blink "*4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'Your\'re swell'
