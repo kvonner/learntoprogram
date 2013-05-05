5c302495fb87916a4c2b6a41e7f7c7f561cb8174
Kristian von Rickenbach <kvonrickenbach@gmail.com>
2013-05-05 14:41:17 -0700

    Chapter 5 exercises

---
+++ chapter5.rb
@@ -0,0 +1,12 @@
+#puts 'Hello there, and what\'s your first name?'
+#first_name = gets.chomp
+#puts "and your middlename?"
+#middle_name = gets.chomp
+#puts "and finally last name?"
+#last_name = gets.chomp
+#puts "Well hello " + first_name +" "+middle_name+" "+last_name+", pleased to meet you!"
+
+puts "What's your favorite number?"
+number = gets.chomp
+better_number = number.to_i+1
+puts "Don't you think "+better_number.to_s+" is way better?"
